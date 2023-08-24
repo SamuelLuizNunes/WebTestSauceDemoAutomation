86068565fb8aa39439b7d308e07d416d1dca266c
Samuel Nunes <samuelluiznunes10@gmail.com>
2023-08-23 21:51:11 -0300

    Aplicando PageObjects

--- src/main/java/tester/samuel/automation/pages/LoginPage.java
+++ src/main/java/tester/samuel/automation/pages/LoginPage.java
@@ -1,5 +1,6 @@
 package tester.samuel.automation.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class LoginPage {
@@ -9,5 +10,20 @@ public class LoginPage {
         this.driver = driver;
     }
 
+    public void inputUserName(String username){
+        driver.findElement(By.id("user-name")).sendKeys(username);
+    }
 
+    public void inputPassword(String password){
+        driver.findElement(By.id("password")).sendKeys(password);
+    }
+
+    public void clickLoginButton(){
+        driver.findElement(By.id("login-button")).click();
+    }
+
+    public String getTitleLoggedIn(){
+        String titleLoggedIn =driver.findElement(By.cssSelector(".product_label")).getText();
+        return titleLoggedIn;
+    }
 }
--- src/main/java/tester/samuel/automation/test/Tests.java
+++ src/main/java/tester/samuel/automation/test/Tests.java
@@ -7,16 +7,18 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import tester.samuel.automation.core.Driver;
 import tester.samuel.automation.enums.Browser;
+import tester.samuel.automation.pages.LoginPage;
 
 public class Tests {
     Driver driver;
+    LoginPage loginPage;
 
     @BeforeEach
     public void initialize() {
-        System.setProperty("webdriver.chrome.driver", "driver\\chromedriver.exe");
         driver = new Driver(Browser.CHROME);
         driver.getDriver().get("https://www.saucedemo.com/v1/");
         driver.getDriver().manage().window().maximize();
+        loginPage = new LoginPage(driver.getDriver());
     }
 
     @AfterEach
@@ -26,6 +28,9 @@ public class Tests {
 
     @Test
     public void loginWithValidCredentials() {
-
+        loginPage.inputUserName("standard_user");
+        loginPage.inputPassword("secret_sauce");
+        loginPage.clickLoginButton();
+        Assertions.assertEquals("Products", loginPage.getTitleLoggedIn());
     }
 }
